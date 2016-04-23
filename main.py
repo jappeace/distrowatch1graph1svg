acdd2b111451d9eeddca5eb72da874bbe1480269
Jappie Klooster <jappieklooster@hotmail.com>
2016-04-23 14:41:47 +0200

    added argument parsing  and the right links are now found. program starts to become usefull, decided to probably split this project up into 2 program, 1 that makes a sensible structure out of distro watch, and another that will render the svg

--- main.py
+++ main.py
@@ -18,7 +18,28 @@
 from requests import Session
 from bs4 import BeautifulSoup
 
+import re
 from sys import setrecursionlimit
+
+import argparse
+parser = argparse.ArgumentParser(
+    description=
+        "Distrograph Copyright (C) 2016 Jappie Klooster\n" +
+        "This program comes with ABSOLUTELY NO WARRANTY; for details see the \n" +
+        "LICENSE file. This is free software, and you are welcome to \n" +
+        "redistribute it under certain conditions; see the LICENSE file for details"
+    )
+parser.add_argument('--baseurl', default="https://distrowatch.com/", help="default http://distrowatch.com")
+parser.add_argument('--searchOptions', 
+        default="ostype=All&category=All&origin=All&basedon=All&notbasedon=None&desktop=All&architecture=All&package=All&rolling=All&isosize=All&netinstall=All&status=All", 
+        help="the GET form generates this at distrowatch.com/search.php,"+
+        "everything behind the ? can be put in here, "+
+        "use this to add constraints to your graph, for example if you're "+
+        "only interested in active distro's, specify it at the form and copy "+
+        "the resulting GET request in this argument")
+
+args = parser.parse_args()
+
 setrecursionlimit(10000)
 
 def tohtml(lines, outFile = "output.html"):
@@ -26,11 +47,16 @@ def tohtml(lines, outFile = "output.html"):
         f.writelines(lines)
 
 session = Session()
-baseurl = "https://distrowatch.com/"
+baseurl = args.baseurl
 
 website = session.get(
-        baseurl + 'search.php?ostype=All&category=All&origin=All&basedon=All&notbasedon=None&desktop=All&architecture=All&package=All&rolling=All&isosize=All&netinstall=All&status=All'
+        baseurl + 'search.php?%s' % args.searchOptions
     ).text
 soup = BeautifulSoup(website, 'html.parser')
 
-print(soup.body.find(class_="Logo").find(class_="News").find_all("tr")[2].find("td").get_text()) # .News tr
+def tagfilter(tag):
+    return tag.name == "b" and re.match("[0-9]+\.", tag.text)
+print('---')
+for element in soup.find_all(tagfilter):
+    print(element)
+print() # .News tr
