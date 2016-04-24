a1406ac11ff8c55135eb49d596e354f6a8c83c2e
Jappie Klooster <jappieklooster@hotmail.com>
2016-04-24 14:43:17 +0200

    first level of children can now be attached

---
+++ graph.py
@@ -0,0 +1,80 @@
+# This program tries to parse distrowatch and create a svg graph simliar to: <https://en.wikipedia.org/wiki/Linux_distribution#/media/File:Linux_Distribution_Timeline_with_Android.svg>
+# Copyright (C) 2016 Jappe Klooster
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.If not, see <http://www.gnu.org/licenses/>.
+
+
+
+import argparse
+parser = argparse.ArgumentParser(
+    description=
+        "Distrograph Copyright (C) 2016 Jappie Klooster\n" +
+        "This program comes with ABSOLUTELY NO WARRANTY; for details see the \n" +
+        "LICENSE file. This is free software, and you are welcome to \n" +
+        "redistribute it under certain conditions; see the LICENSE file for details\n"+
+        "--\n"
+    )
+parser.add_argument(
+    'jsonInput',
+    nargs='+',
+    help="The structured json from fetchdists.py"
+)
+
+args = parser.parse_args()
+
+import json 
+
+son = ''.join(args.jsonInput)
+categories = json.loads(son)
+
+# using a lot of strings from te JSON strucutre,
+# better group them in a structure
+class strings:
+    independend = "Independent"
+    based = "Basedon"
+    name = "Name"
+    children = "Children"
+
+independents = filter(
+    lambda x: x[strings.based] == strings.independend,
+    categories
+)
+
+def listToDict(keyFunction, values):
+    return dict((keyFunction(v), v) for v in values)
+
+# A list is just a great way to waste time for this usecase
+independents = listToDict(lambda x: x["Name"], independents)
+
+def deepen(collection):
+    if(len(collection) == 0):
+        return True
+    current = collection[0]
+    bases = current[strings.based].split(",")
+    for base in bases:
+        if base in independents:
+            parent = independents[base]
+            if not strings.children in parent:
+                parent[strings.children] = []
+            parent[strings.children].append(current)
+        else:
+            print("Ignoring %s for %s" % (base, current[strings.name]))
+    return deepen(collection[1:])
+
+notindependents = list(filter(lambda x: not x[strings.based] == strings.independend, categories))
+def printjson(item):
+    print(json.dumps(item, indent=4))
+deepen(notindependents )
+for key,child in independents.items():
+    printjson(child)
---
+++ graph.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+python graph.py `cat out/dists.json`
