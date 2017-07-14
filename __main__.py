b21b8c739b328de29aadb63ea994f17ad058c058
Jappie Klooster <jappieklooster@hotmail.com>
2017-07-14 21:17:07 +0200

    Pylint and some cleanup

--- __main__.py
+++ __main__.py
@@ -29,59 +29,66 @@ import os
 import argparse
 import json
 
-parser = argparse.ArgumentParser(
-    description=
-        "Distrograph Copyright (C) 2016 Jappie Klooster\n" +
-        "This program comes with ABSOLUTELY NO WARRANTY; for details see the \n" +
-        "LICENSE file. This is free software, and you are welcome to \n" +
-        "redistribute it under certain conditions; see the LICENSE file for details\n"+
-        "--\n"
+def main():
+    parser = argparse.ArgumentParser(
+        description=""" Distrograph Copyright (C) 2016 Jappie Klooster
+                        This program comes with ABSOLUTELY NO WARRANTY;
+                        for details see the LICENSE file.
+                        This is free software,
+                        and you are welcome to redistribute it under certain
+                        conditions; see the LICENSE file for details"""
+        )
+    parser.add_argument(
+        '--baseurl',
+        default="https://distrowatch.com",
+        help="default http://distrowatch.com"
     )
-parser.add_argument(
-    '--baseurl',
-    default="https://distrowatch.com",
-    help="default http://distrowatch.com"
-)
-parser.add_argument(
-    '--searchOptions',
-    default="ostype=All&category=All&origin=All&basedon=All&notbasedon=None"+
-    "&desktop=All&architecture=All&package=All&rolling=All&isosize=All"+
-    "&netinstall=All&status=All",
-    help="the GET form generates this at distrowatch.com/search.php,"+
-    "everything behind the ? can be put in here, "+
-    "use this to add constraints to your graph, for example if you're "+
-    "only interested in active distro's, specify it at the form and copy "+
-    "the resulting GET request in this argument"
-)
+    parser.add_argument(
+        '--searchOptions',
+        default="ostype=All&category=All&origin=All&basedon=All" +
+        "&notbasedon=None" +
+        "&desktop=All&architecture=All&package=All&rolling=All&isosize=All" +
+        "&netinstall=All&status=All",
+        help="""the GET form generates this at distrowatch.com/search.php
+                everything behind the ? can be put in here,
+                use this to add constraints to your graph,
+                for example if you're only interested in active distro's,
+                specify it at the form and copy the resulting GET request in
+                this argument"""
+    )
 
-args = parser.parse_args()
-os.chdir(os.path.dirname(os.path.realpath(__file__)))
-outputdir = "out"
-if not os.path.isdir(outputdir):
-    os.mkdir(outputdir)
-os.chdir(outputdir)
+    args = parser.parse_args()
+    os.chdir(os.path.dirname(os.path.realpath(__file__)))
+    outputdir = "out"
+    if not os.path.isdir(outputdir):
+        os.mkdir(outputdir)
+    os.chdir(outputdir)
 
-fetched_dists_file = "dists.json"
-son = ""
-if os.path.isfile(fetched_dists_file):
-    with open(fetched_dists_file, "r") as cached:
-        print("using cached file %s/%s" % (outputdir, fetched_dists_file))
-        son = "".join(cached.readlines())
-if son == "":
-    url = args.baseurl
-    print("fetching distros from %s" % url)
-    son = fetch_dist_list_from(url, args.searchOptions)
-    with open(fetched_dists_file, "w") as cached:
-        print("wrote cache file %s/%s" % (outputdir, fetched_dists_file))
-        cached.write(son)
+    fetched_dists_file = "dists.json"
+    son = ""
+    if os.path.isfile(fetched_dists_file):
+        with open(fetched_dists_file, "r") as cached:
+            print("using cached file %s/%s" % (outputdir, fetched_dists_file))
+            son = "".join(cached.readlines())
+    if son == "":
+        url = args.baseurl
+        print("fetching distros from %s" % url)
+        son = fetch_dist_list_from(url, args.searchOptions)
+        with open(fetched_dists_file, "w") as cached:
+            print("wrote cache file %s/%s" % (outputdir, fetched_dists_file))
+            cached.write(son)
 
-son = to_graph(son)
+    son = to_graph(son)
 
-csv = toCSV(json.loads(son), "").result
-csvfile = "dists.csv"
-print("writing csv to %s/%s" % (outputdir, csvfile))
-with open(csvfile, "w") as cached:
-    cached.write(csv)
-os.chdir("../")
-call("gnuclad %s/%s dists.svg gnuclad.conf" % (outputdir, csvfile ), shell=True)
-call("inkscape -z -e dists.png dists.svg", shell=True)
+    csv = toCSV(json.loads(son), "").result
+    csvfile = "dists.csv"
+    print("writing csv to %s/%s" % (outputdir, csvfile))
+    with open(csvfile, "w") as cached:
+        cached.write(csv)
+    os.chdir("../")
+    call("gnuclad %s/%s dists.svg gnuclad.conf" % (outputdir, csvfile),
+         shell=True)
+    call("inkscape -z -e dists.png dists.svg", shell=True)
+
+if __name__ == "__main__":
+    main()
